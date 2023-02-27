e02770addb9b28d07616b68f82c11f09fb3a2ea7
GusEngers <gustavoengers6441@gmail.com>
2023-02-27 17:23:18 -0300

    Proyecto 3 completo

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Dichos proyectos emplean funcionalidades propias de Python y librerías tanto es
 
 - [Mad Libs](https://github.com/GusEngers/python-projects-for-beginners/tree/main/proyecto-1) →  `Completo`
 - [Adivina el número (computadora)](https://github.com/GusEngers/python-projects-for-beginners/tree/main/proyecto-2) →  `Completo`
-- [Adivina el número (usuario)](#) →  `En proceso`
+- [Adivina el número (usuario)](https://github.com/GusEngers/python-projects-for-beginners/tree/main/proyecto-3) →  `Completo`
 - [Piedra, Papel, Tijeras](#) →  `En proceso`
 - [Hangman](#) →  `En proceso`
 - [Temporizador de cuenta regresiva](#) →  `En proceso`
---
+++ proyecto-3/main.py
@@ -0,0 +1,22 @@
+import random
+
+if __name__ == "__main__":
+    max_number = int(input("Ingrese el rango máximo (mayor a 1) que desee usar: "))
+    min_number = 1
+    feedback = ""
+
+    while max_number <= 1:
+        max_number = int(input("El rango máximo debe ser mayor a 1. Ingrese el rango máximo: "))
+
+    print(f"Piensa en un número entre 1 y {max_number}")
+    print("Responde con:\n → 'A' si el  número es más alto al que pensaste\n → 'B' si es más bajo al que pensaste\n → 'C' si es el número que pensaste")
+    while feedback != "C":
+        guess = random.randint(min_number, max_number)
+        feedback = input(f"{guess} es el número que pensaste? ").upper()
+        
+        if feedback == "A":
+            max_number = guess - 1
+        if feedback == "B":
+            min_number = guess + 1
+            
+    print("Si, he acertado el número!!")
