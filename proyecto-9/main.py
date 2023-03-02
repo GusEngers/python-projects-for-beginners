ebed30308fb01418f640554ea284ed45d3046591
GusEngers <gustavoengers6441@gmail.com>
2023-03-02 19:38:47 -0300

    Create main.py

---
+++ proyecto-9/main.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    player_one = "x"
+    player_two = "◯"
+
+    one_tg, two_tg, three_tg, four_tg, five_tg, six_tg, seven_tg, eight_tg, nine_tg = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    table_game = f""" 
+      {one_tg} | {two_tg} | {three_tg}
+     -----------
+      {four_tg} | {five_tg} | {six_tg} 
+     -----------
+      {seven_tg} | {eight_tg} | {nine_tg}
+    """
+    
